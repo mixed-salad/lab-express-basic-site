f6887759f4c359a0234fdcf83204e5a41dfe03bf
Harumi Terayama <hterayama@gmail.com>
2020-12-12 16:55:20 +0100

    added hbs structure

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 
 app.get('/home', function(request, response) {
-    response.sendFile(__dirname + '/views/home.html');
+    response.render('home');
 });
 app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/views/home.html');
+    response.render('home');
 });
 app.get('/about', function(request, response) {
-    response.sendFile(__dirname + '/views/about.html');
+    response.render('about');
 });
 app.get('/works', function(request, response) {
-    response.sendFile(__dirname + '/views/works.html');
+    response.render('works');
 });
 app.get('/gallery', function(request, response) {
-    response.sendFile(__dirname + '/views/gallery.html');
+    response.render('gallery');
 });
 
 app.listen('3000');
